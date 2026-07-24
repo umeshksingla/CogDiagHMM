cc2ab284e061a2509daaf99c2ecb29c35d6158af
Umesh Singla <umeshksingla@gmail.com>
2026-07-23 22:11:31 -0700

    changes to base data class

--- data_utils.py
+++ data_utils.py
@@ -4,7 +4,9 @@ from domains.seasonstaskdata import SeasonsTaskData
 from domains.cyclicfwdtaskdata import CyclicFwdTaskData
 from domains.nbackrnndata import NBackRNNData
 from domains.cyclicfwdrnndata import CyclicFwdRNNData
-from domains.io_utils import save_data
+from domains.hierarchytaskdata import HierarchicalCueTaskData
+from domains.orderedtaskdata import OrderedTaskData
+from domains.utilities.io_utils import save_data
 
 
 def construct_data(task, BATCHES, STEPS, N_OBS_DIM=None):
@@ -20,22 +22,27 @@ def construct_data(task, BATCHES, STEPS, N_OBS_DIM=None):
         gen_model = CyclicFwdTaskData(n_states=4, n_inputs=1, n_obs_dim=2)
     elif task == 'cyclicfwdrnn':
         gen_model = CyclicFwdRNNData(n_states=4, n_inputs=1)
+    elif task == 'hierarchicalcuetask':
+        gen_model = HierarchicalCueTaskData(n_states=7, n_inputs=1, n_obs_dim=2)
+    elif task == 'orderedtask':
+        gen_model = OrderedTaskData(n_states=5, n_inputs=1, n_obs_dim=2)
     else:
         raise ValueError('Unknown task')
     print(gen_model.__class__.__name__)
-    inputs, stim_seqs, true_states, observations, _, task_config = gen_model.generate(n_batches=BATCHES, n_steps=STEPS)
-    print("Inputs:", inputs.shape, "stim_seqs:", stim_seqs.shape, "true_states:", true_states.shape, "observations:", observations.shape)
-    print("inputs", inputs[0, :10])
+    stim_seqs, resp_seqs, true_states, observations, _, task_config = gen_model.generate(n_batches=BATCHES, n_steps=STEPS)
+    print("stim_seqs:", stim_seqs.shape, "resp_seqs:", resp_seqs, "true_states:", true_states.shape, "observations:", observations.shape)
+    print("stim_seqs", stim_seqs[0, :10])
+    print("resp_seqs", resp_seqs[0, :10])
     print("true_states", true_states[0, :10])
-    return inputs, stim_seqs, true_states, observations, task_config
+    return stim_seqs, resp_seqs, true_states, observations, task_config
 
 
 if __name__ == "__main__":
 
     BATCHES = 20
     STEPS = 10000
-    task = 'cyclicfwdrnn'
+    task = 'orderedtask'
     data_path = f'/Users/usingla/research/CogDiagHMM/data/{task}_may4.pkl'
-    inputs, stim_seqs, true_states, observations, task_config = construct_data(task, BATCHES, STEPS)
+    stim_seqs, resp_seqs, true_states, observations, task_config = construct_data(task, BATCHES, STEPS)
+    save_data(data_path, {'stim_seqs': stim_seqs, 'resp_seqs': resp_seqs, 'true_states': true_states, 'observations': observations, 'task_config': task_config})
 
-    save_data(data_path, {'inputs': inputs, 'stim_seqs': stim_seqs, 'true_states': true_states, 'observations': observations, 'task_config': task_config})
--- domains/basedata.py
+++ domains/basedata.py
@@ -13,9 +13,8 @@ class BaseData:
         self.n_inputs = n_inputs
         self.n_obs_dim = n_obs_dim
         self.task_config = task_config
-        pass
 
-    def get_inputs_array(self, n_steps):
+    def get_stim_resp_array(self, n_steps):
         raise NotImplementedError()
 
     def get_initial_state(self, inpt):
@@ -28,47 +27,22 @@ class BaseData:
         raise NotImplementedError()
 
     def generate_one(self, n_steps, btch=None):
-        np.random.seed(0)
-        self.n_steps = n_steps
-        inputs = self.get_inputs_array(n_steps)
-        stim_seq = inputs.copy()
-        states = np.zeros(n_steps, dtype=int)
-        observations = np.zeros((n_steps, self.n_obs_dim))
-        true_transition_matrices = []
-
-        # Initial state (random)
-        states[0] = self.get_initial_state(inputs[0])
-        observations[0] = self.get_observation_t(states[0], inputs[0])
-
-        # 2. Simulation Loop
-        for t in range(1, n_steps):
-            prev_state = states[t - 1]
-            current_input = inputs[t]  # Input driving the transition to t
-
-            # Calculate Dynamic Transition Matrix
-            A_t = self.get_transition_matrix(current_input)
-            true_transition_matrices.append(A_t)
-
-            # Sample Next State
-            states[t] = np.random.choice(self.n_states, p=A_t[prev_state])
-
-            # Sample Observation
-            observations[t] = self.get_observation_t(states[t], current_input)
-        return inputs, stim_seq, states, observations, true_transition_matrices
+        raise NotImplementedError()
 
     def generate(self, n_batches=1, n_steps=100):
-        inputs, stim_seqs, states, observations, true_transition_matrices = [], [], [], [], []
+        stim_seqs, resp_seqs, states, observations, true_transition_matrices = [], [], [], [], []
         for _ in range(n_batches):
+            print(f"Generating batch {_}")
             np.random.seed(_)
-            inputs_, stim_seq_, states_, observations_, true_transition_matrices_ = self.generate_one(n_steps, _)
-            inputs.append(inputs_)
+            stim_seq_, resp_seq_, states_, observations_, true_transition_matrices_ = self.generate_one(n_steps, _)
             stim_seqs.append(stim_seq_)
+            resp_seqs.append(resp_seq_)
             states.append(states_)
             observations.append(observations_)
             true_transition_matrices.append(true_transition_matrices_)
-        inputs = np.array(inputs)
         stim_seqs = np.array(stim_seqs)
+        resp_seqs = np.array(resp_seqs)
         states = np.array(states)
         observations = np.array(observations)
         true_transition_matrices = np.array(true_transition_matrices)
-        return inputs, stim_seqs, states, observations, true_transition_matrices, self.task_config
+        return stim_seqs, resp_seqs, states, observations, true_transition_matrices, self.task_config
