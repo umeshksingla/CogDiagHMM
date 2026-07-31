9a54a0526650883e965ea41827cca6f6d3347719
Umesh Singla <umeshksingla@gmail.com>
2026-07-30 22:48:59 -0700

    add diag ghmm class

---
+++ hmmmodels/DiagGHMM.py
@@ -0,0 +1,11 @@
+from hmmmodels.GHMM import GHMM
+
+from dynamax.hidden_markov_model import DiagonalGaussianHMM
+
+
+class DiagGHMM(GHMM):
+    prefix = 'dgHMM'
+
+    def __init__(self, num_states, emission_dim, seed=10):
+        super().__init__(num_states, emission_dim)
+        self.hmm = DiagonalGaussianHMM(self.num_states, self.emission_dim)
